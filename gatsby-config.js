c151989a6013b1cc184d52ddd82e0c5bdaee4e71
anastasius <antsdevp@gmail.com>
2020-06-04 20:25:43 +0700

    add react-helmet and graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Frontend Maters Gatsby workshop',
+    description: 'A site we built together from FEM',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
