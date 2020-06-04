b6c65600f9833ac35c1878bf8cfcd93466af6034
anastasius <antsdevp@gmail.com>
2020-06-04 10:14:01 +0700

    router in gatsby use reach link

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const about = () => (
+  <>
+    <h1>About Me</h1>
+    <p>This is my personal website</p>
+    <Link to="/">go home</Link>
+  </>
+);
+
+export default about;
