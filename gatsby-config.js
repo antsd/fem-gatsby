0c84d2895b26c128ab4517ef63a0618525e53e05
anastasius <antsdevp@gmail.com>
2020-06-05 08:24:27 +0700

    add mdx and configure

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: 'Frontend Maters Gatsby workshop',
     description: 'A site we built together from FEM',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
