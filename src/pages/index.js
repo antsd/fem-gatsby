043c404637d0e4aeb103970a473ad16c04125bf7
anastasius <antsdevp@gmail.com>
2020-06-05 20:57:55 +0700

    add hero image header

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,23 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>I am Anastasius</p>
-      <Link to="/about/">Learn about me</Link>
+    <>
+      <Hero />
+      <Layout>
+        <h1>Home</h1>
+        <p>I am Anastasius</p>
+        <Link to="/about/">Learn about me</Link>
 
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
